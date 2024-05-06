5cc0616acef5a668a28bf3a3621d4210e86331e5
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-05-06 16:39:08 -0300

    criando controller para users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   resource :auths, only: [:create]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
