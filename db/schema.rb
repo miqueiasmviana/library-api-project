98a6ed6d560295c251c1e69f89663e431292616d
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-05-07 14:12:42 -0300

    gerando migração de referencia

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_170920) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_171142) do
   create_table "books", force: :cascade do |t|
     t.string "cover"
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_170920) do
     t.date "first_published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "kind_id", null: false
+    t.index ["kind_id"], name: "index_books_on_kind_id"
   end
 
   create_table "kinds", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_170920) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "books", "kinds"
 end
