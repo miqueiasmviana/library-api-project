f88eccbf37e6b2202ad08670aad5bd75b6d23b05
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-05-09 15:36:49 -0300

    adicionando mime_types

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::Base
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  
+  before_action :set_headers
+
+  def set_headers
+    response.headers['Content-Type'] = 'application/vnd.api+json'
+  end
+
+  def ensure_json_request
+    if request.get? || request.delete?
+      return if request.headers['Accept'] =~ /vnd\.api\+json/
+      render nothing: true, status: 406
+    else
+      if (request.headers['Content-Type'] =~ /vnd\.api\+json/) &&
+         (request.headers['Accept'] =~ /vnd\.api\+json/)
+        return
+      end
+   
+      render nothing: true, status: 415
+    end
+  end
 end
